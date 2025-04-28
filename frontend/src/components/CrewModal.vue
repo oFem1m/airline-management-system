9019f76c84a6d543cf541d20346a7c6cafc901c3
alex <volohovaleksandr1974@gmail.com>
2025-04-28 21:48:01 +0300

    CrewModal updated

--- frontend/src/components/CrewModal.vue
+++ frontend/src/components/CrewModal.vue
@@ -3,31 +3,27 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title">Назначить сотрудника</h5>
-                    <button
-                        type="button"
-                        class="btn-close"
-                        aria-label="Close"
-                        @click="close"
-                    ></button>
+                    <h5 class="modal-title">Назначение экипажа</h5>
+                    <button type="button" class="btn-close" @click="close"></button>
                 </div>
                 <div class="modal-body">
-                    <form @submit.prevent="submitForm">
-                        <div class="mb-3">
-                            <label for="employeeId" class="form-label">Выберите сотрудника</label>
-                            <select
-                                id="employeeId"
-                                v-model="selectedEmployee"
-                                class="form-control"
-                                required
-                            >
-                                <option v-for="emp in employees" :key="emp.id" :value="emp.id">
-                                    {{ emp.first_name }} {{ emp.last_name }}
-                                </option>
-                            </select>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Назначить</button>
-                    </form>
+                    <label class="form-label">Выберите сотрудников</label>
+                    <div v-for="emp in employees" :key="emp.id" class="form-check">
+                        <input
+                            class="form-check-input"
+                            type="checkbox"
+                            :id="'emp-' + emp.id"
+                            :value="emp.id"
+                            v-model="selected"
+                        />
+                        <label class="form-check-label" :for="'emp-' + emp.id">
+                            {{ emp.first_name }} {{ emp.last_name }} (ID: {{ emp.id }})
+                        </label>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" @click="close">Отмена</button>
+                    <button class="btn btn-primary" @click="submitForm">Сохранить</button>
                 </div>
             </div>
         </div>
@@ -35,65 +31,50 @@
 </template>
 
 <script>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, watch, defineComponent } from 'vue'
 import { Modal } from 'bootstrap'
 import employeeApi from '@/API/employeeApi'
 
-export default {
+export default defineComponent({
     name: 'CrewModal',
     props: {
-        flightId: {
-            type: Number,
-            required: true,
-        },
+        flightId: { type: Number, required: true },
+        initialCrew: { type: Array, default: () => [] },
     },
-    emits: ['addCrewMember'],
+    emits: ['updateCrew'],
     setup(props, { emit }) {
         const employees = ref([])
-        const selectedEmployee = ref(null)
+        const selected = ref([])
         const modalElement = ref(null)
         let modalInstance = null
 
-        const fetchEmployees = () => {
-            employeeApi
-                .getEmployees()
-                .then((response) => {
-                    employees.value = response.data
-                })
-                .catch(console.error)
-        }
+        const fetchEmployees = () =>
+            employeeApi.getEmployees().then((r) => (employees.value = r.data))
+
+        watch(
+            () => props.initialCrew,
+            (crew) => {
+                selected.value = crew.map((m) => m.employee_id)
+            },
+            { immediate: true },
+        )
 
         const open = () => {
-            if (!modalInstance) {
-                modalInstance = new Modal(modalElement.value)
-            }
+            if (!modalInstance) modalInstance = new Modal(modalElement.value)
             modalInstance.show()
         }
-
-        const close = () => {
-            if (modalInstance) {
-                modalInstance.hide()
-            }
-        }
+        const close = () => modalInstance && modalInstance.hide()
 
         const submitForm = () => {
-            emit('addCrewMember', { flightId: props.flightId, employeeId: selectedEmployee.value })
+            emit('updateCrew', selected.value)
             close()
         }
 
-        onMounted(() => {
-            fetchEmployees()
-        })
+        onMounted(fetchEmployees)
 
-        return {
-            employees,
-            selectedEmployee,
-            open,
-            close,
-            submitForm,
-        }
+        return { employees, selected, modalElement, open, close, submitForm }
     },
-}
+})
 </script>
 
 <style scoped></style>
